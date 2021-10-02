358dde933895181c44c23bb0428a841f2e02b8d2
Dag Sommer <dagaleks@me.com>
2021-10-02 18:49:04 +0200

    fixed everything

--- Scraper.js
+++ Scraper.js
@@ -1,17 +1,34 @@
 import PDFJS from 'pdfjs-dist'
 import fetch from 'node-fetch'
 import { JSDOM } from 'jsdom'
+import Puppeteer from 'puppeteer';
 
-const MAX_DEPTH = 1
+const MAX_DEPTH = 2
+
 class ScraperLocation {
-	/* fileURL
-	textExcerpt
-	constructor(url, exceprt) {
+
+	constructor(url, exceprt, pageNumber) {
 		this.fileURL = url
 		this.textExcerpt = exceprt
-	} */
+		this.pageNumber = pageNumber
+	}
+	static n(url, exceprt, pageNumber) {
+		let sl = new ScraperLocation(url, exceprt, pageNumber)
+		return sl
+	}
+	pretty() {
+		return `${this.fileURL} ${!!this.pageNumber ? "side " + this.pageNumber : ""}: ${this.textExcerpt}`
+	}
 }
 
+function streamToString(stream) {
+	const chunks = [];
+	return new Promise((resolve, reject) => {
+		stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+		stream.on('error', (err) => reject(err));
+		stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+	})
+}
 class Scraper {
 
 	constructor(url, phraseToFind) {
@@ -25,54 +42,100 @@ class Scraper {
 		this.phraseToFind = phraseToFind
 		this.locations = []
 		this.visitedURLs = []
+		this.browser = null
+		this.page = null
+		this.regex = new RegExp(
+			"(.{0,20})" + this.phraseToFind + "(.{0,20})",
+			"gi"
+		)
 	}
 
-	start() {
-		return new Promise((resolve/*, reject*/) => {
-			this.getPage(this.url, 0).then((val)=>{
-				console.log(this.locations)
-				for (let loc of this.locations) {
-					console.log(`${loc.url}: found text: ${loc.text}`)
-				}
-				resolve()
-			})
+	async start() {
+		this.browser = await Puppeteer.launch({
+			headless: false,
 		})
+		this.page = await this.browser.newPage()
+		await this.page.setRequestInterception(true)
+		this.page.on('request', (req) => {
+			if (req.resourceType() == 'font' || req.resourceType() == 'image') {
+				req.abort();
+			}
+			else {
+				req.continue();
+			}
+		})
+
+		await this.getPage(this.url, 0)
+		for (let loc of this.locations) {
+			console.log(`${loc.pretty()}`)
+		}
+		this.browser.close()
 	}
 
 	async getPage(url, depth) {
-		if (depth>MAX_DEPTH) {
+		var validExtension = false
+		let extensions = ["pdf", "html", "/"]
+		extensions.forEach((val) => {
+			if (val.includes(url.substring(url.length - val.length, url.length))) validExtension = true
+		})
+		if (!validExtension) {
+			//console.log("Skipping page with extension: " + url.substring(url.length - 4, url.length))
+			return
+		}
+		if (depth > MAX_DEPTH) {
 			return
 		}
 		try {
 			if (this.visitedURLs.includes(url)) return
 			let res = await (await fetch(url)).text()
+
+			await this.page.goto(url, {
+				waitUntil: 'networkidle0',
+			})
+			//await this.page.waitForNavigation()
 			this.visitedURLs.push(url)
 			await this.parsePage(url, res, depth);
 		} catch (e) {
-			console.error("Got error in getPage: "+e)
+			console.error("Got error in getPage: " + e)
 			return
 		}
 	}
 
-	async parsePage(currentURL, htmlContent, depth) {
+	async parsePage(currentURL, _, depth) {
+
+
+		/* await this.page.evaluate(() => {
+			let elements = document.getElementsByClassName('accordion');
+			for (let element of elements)
+				element.click()
+		}); */
+		const handles = await this.page.$$('.accordion');
+		for (const handle of handles) {
+			await handle.evaluate(b => b.click());
+		}
+		await this.page.waitForNetworkIdle()
+
+		let pageContent = await this.page.$eval("body", el => el.textContent);
+		let htmlContent = await this.page.content()
+		//console.log(pageContent.replace(/\s/g,''))
+		//var regex = new RegExp("\\s?([^\\s]+\\s" + this.phraseToFind + "\\s[^\\s]+)\\s?", "i");
+
+		let matches = pageContent.match(this.regex)
+
 		let dom = new JSDOM(htmlContent)
 		//.querySelector("a").textContent
 		//Check page for contents
 
-		if (!dom.window.document.body.textContent) {
-			return
-		}
-		var regex = new RegExp("\\s?([^\\s]+\\s" + this.phraseToFind + "\\s[^\\s]+)\\s?", "i");
 		// console.log(dom.window.document.body.textContent.replace(/\s/g,''))
 		// at this point, the line above is the same as: var regex =
 
 		//let matches = "Det som virkelig suger er korona fordi det er det verste som finnes".match(regex)
- 		let matches = dom.window.document.body.textContent.match(regex)
-		dom.window.document.body.content
+		//let matches = dom.window.document.body.textContent.match(regex)
+
 		if (!!matches && matches.length > 0) {
 			for (let match of matches) {
-				console.log("Found match: "+match)
-				this.locations.push({ url: currentURL, text: match})
+				let loc = ScraperLocation.n(currentURL, match)
+				this.locations.push(loc)
 			}
 		}
 
@@ -83,16 +146,23 @@ class Scraper {
 			if (!param || !param.getAttribute("href")) {
 				continue
 			}
-			console.log("Found <a> tag with href: "+param.getAttribute("href").split("?")[0].split("#")[0])
+			//console.log("Found <a> tag with href: " + param.getAttribute("href").split("?")[0].split("#")[0])
 			let url = param.getAttribute("href").split("?")[0].split("#")[0] + ""
 			//console.log("slashes: "+this.url.match(/\//g).length)
-			let compareURL = this.url.match(/\//g).length > 2 ? this.url.substr(0, this.url.lastIndexOf("/")) : this.url
-			var t = "\\some\\route\\here";
+			let compareURL = currentURL.match(/\//g).length > 2 ? currentURL.substr(0, currentURL.lastIndexOf("/")) : currentURL
 			//console.log("includes comp url (" + compareURL + "): "+url.includes(compareURL))
 			if (url.includes(compareURL)) {
-				//console.log("going to url: " + url)
-				if (url.substring(url.length-3, url.length-1) == "pdf") {
-					console.log("File is PDF!!")
+				let currentLast = currentURL.split("/").pop()
+				let currentIsIndex = currentLast.includes("index") && currentURL.substring(0, currentURL.length - currentLast.length) == url
+				let newLast = currentURL.split("/").pop()
+				let newIsIndex = newLast.includes("index") && url.substring(0, url.length - newLast.length) == currentURL
+				if (this.visitedURLs.includes(url) || currentIsIndex || newIsIndex) continue
+
+				// console.log("going to url: " + url)
+				// console.log("with ending: "+url.substring(url.length - 3, url.length))
+				if (url.substring(url.length - 3, url.length) == "pdf") {
+					await this.getPDFResults(url)
+					continue
 				}
 				await this.getPage(url, depth + 1)
 			}
@@ -116,63 +186,30 @@ class Scraper {
 		return (null);
 	}
 
-
-
-	findInPDF(url) {
-		var searchText = "JavaScript"
-		function searchPage(doc, pageNumber) {
-			return doc
-				.getPage(pageNumber)
-				.then(function (page) {
-					return page.getTextContent()
+	async getPDFResults(url) {
+		let doc = await PDFJS.getDocument({
+			url,
+			verbosity: 0,
+		}).promise
+		for (var i = 1; i <= doc.numPages; i++) {
+			let page = await doc.getPage(i)
+			let content = await page.getTextContent()
+			// Search combined text content using regular expression
+			var text = content.items
+				.map(function (i) {
+					return i.str
 				})
-				.then(function (content) {
-					// Search combined text content using regular expression
-					var text = content.items
-						.map(function (i) {
-							return i.str
-						})
-						.join("")
-					var re = new RegExp(
-						"(.{0,20})" + searchText + "(.{0,20})",
-						"gi"
-					),
-						m
-					var lines = []
-					while ((m = re.exec(text))) {
-						var line =
-							(m[1] ? "..." : "") + m[0] + (m[2] ? "..." : "")
-						lines.push(line)
-					}
-					return { page: pageNumber, items: lines }
-				})
+				.join("")
+			var m
+			while ((m = this.regex.exec(text))) {
+				var line =
+					(m[1] ? "..." : "") + m[0] + (m[2] ? "..." : "")
+				this.locations.push(ScraperLocation.n(url, line, i))
+			}
 		}
-
-		var loading = PDFJS.getDocument(url)
-		loading.promise
-			.then(function (doc) {
-				var results = []
-				for (var i = 1; i <= doc.numPages; i++)
-					results.push(searchPage(doc, i))
-				return Promise.all(results)
-			})
-			.then(function (searchResults) {
-				// Display results using divs
-				searchResults.forEach(function (result) {
-					var div = document.createElement("div")
-					div.className = "pr"
-					document.body.appendChild(div)
-					div.textContent = "Page " + result.page + ":"
-					result.items.forEach(function (s) {
-						var div2 = document.createElement("div")
-						div2.className = "prl"
-						div.appendChild(div2)
-						div2.textContent = s
-					})
-				})
-			})
-			.catch(console.error)
 	}
+
+
 }
 
 export { Scraper, ScraperLocation }
